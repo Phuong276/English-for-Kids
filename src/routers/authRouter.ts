716b2ac325ecfef0af600feca1af830686f51b09
phuongvd-dipro <phuongvd@dipro.vn>
2023-05-14 21:55:48 +0700

    fix login

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authController } from "../controllers";
 
 const authRouter = Router({ mergeParams: true });
-authRouter.get("/api/login", authController.login);
+authRouter.post("/api/login", authController.login);
 authRouter.post("/api/register", authController.register);
 
 export default authRouter;
